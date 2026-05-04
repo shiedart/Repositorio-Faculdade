92c23abeafce48e42dfddf0a32e40211be4dc841
Cauã Felipe <140278729+shiedart@users.noreply.github.com>
2026-05-04 11:47:58 -0300

    up mini att no cad
    trocando login por cadastro e linkando o cadastro no nav, add nav na tela de cadastro, mini att no css da pag cad

---
+++ projetoads1/projeto/js/darkmode.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("toggleDark");
+
+// carregar preferência
+if (localStorage.getItem("dark-mode") === "true") {
+    document.body.classList.add("dark-mode");
+    button.textContent = "☀️";
+}
+
+// clique
+button.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    const isDark = document.body.classList.contains("dark-mode");
+
+    // trocar ícone
+    button.textContent = isDark ? "☀️" : "🌙";
+
+    // salvar
+    localStorage.setItem("dark-mode", isDark);
+});
